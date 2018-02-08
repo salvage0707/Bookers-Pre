1b07e47e8bc7fe443c59523d2dce476802db4b1a
Masaki Ikegami <salvage0707@gmail.com>
2018-02-08 14:42:36 +0900

    make top new

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,31 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
+
